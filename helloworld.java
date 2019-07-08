8cf7a514974954ca047c27c8ba2e2082b6a4376c
Pradeep <root@ip-172-31-38-253.ap-south-1.compute.internal>
2019-07-08 07:03:19 +0000

    my first commit

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+ 
+             public static void main(String args[]) {
+	     // Prints "Hello World" to terminal
+             System.out.println("Hello, World"); 
+             // Change #2                        
+             } 
+            } 
