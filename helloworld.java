b2e10d773371d5a6f1e73247f01aeb66d5414bd2
Pradeep <root@ip-172-31-38-253.ap-south-1.compute.internal>
2019-07-10 03:01:25 +0000

    commiting to new branch

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
              public static void main(String args[]) {
 	     // Prints "Hello World" to terminal
              System.out.println("Hello, World");
-	     hello new change 
+	     hello new branch dev 
              // Change #2
              // change #3                        
              } 
